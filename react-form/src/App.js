0d10607f7f0ee55668eb085b9ab6ced651348da7
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-09 07:39:08 +0300

    Setting up

---
+++ react-form/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-3xl text-blue-900 font-bold">App</h1>
+    </div>
+  );
+}
+
+export default App;
