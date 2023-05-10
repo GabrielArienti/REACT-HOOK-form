4702f30f05f655b32a6aed9a37c548842ce91f69
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-10 09:08:49 +0300

    Components

---
+++ react-form/src/components/box/box.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const box = ReactDOM.createRoot(document.getElementById("box"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
---
+++ react-form/src/components/form/form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const form = ReactDOM.createRoot(document.getElementById("form"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
