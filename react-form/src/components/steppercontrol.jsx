cb55db74ea714b26fca2f0021e977969d1e8d481
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-12 14:15:54 +0300

    Stepper Control

--- react-form/src/components/steppercontrol.jsx
+++ react-form/src/components/steppercontrol.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const steppercontrol = () => {
-  return <div className="container mt-5"></div>;
+  return (
+    <div className="container flex justify-around mt-5 mb-8">
+      {/* back button */}
+
+      {/* next button */}
+    </div>
+  );
 };
 
 export default steppercontrol;
