9dc34d4153b312eb56324d34fb2bf644a0f5ad84
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-13 09:59:20 +0300

    Stepper control

--- react-form/src/components/steppercontrol.jsx
+++ react-form/src/components/steppercontrol.jsx
@@ -5,9 +5,17 @@ const steppercontrol = () => {
   return (
     <div className="container flex justify-around mt-5 mb-8">
       {/* back button */}
-      <buttoncontrol>Next</buttoncontrol>
+      <button
+        className="bg-white text-slate-400 uppercase py-2 px-4 rounded-xl font-semibold 
+      cursor-pointer border-2 border-slate-300 hover:bg-slate-300 hover:text-white transition 
+      duration-2oo ease-in-out "
+      ></button>
       {/* next button */}
-      <buttoncontrol>Back</buttoncontrol>
+      <button
+        className="bg-white text-slate-400 uppercase py-2 px-4 rounded-xl font-semibold 
+      cursor-pointer border-2 border-slate-300 hover:bg-slate-300 hover:text-white transition 
+      duration-2oo ease-in-out "
+      ></button>
     </div>
   );
 };
